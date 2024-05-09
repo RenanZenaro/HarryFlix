0ecf224165a92e8b9d1441d33ae28399febac1ad
RenanZenaro <renanzenaro@hotmail.com>
2024-05-08 22:16:02 -0300

    .

--- HarryFlix/Models/Genre.cs
+++ HarryFlix/Models/Genre.cs
@@ -8,9 +8,9 @@ public class Genre
 {
     [Key]
     [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-    public sbyte Id { get; set; }
+    public byte Id { get; set; }
 
-    [Display(Name = "")]
+    [Display(Name = "Nome")]
     [Required(ErrorMessage = "Por favor, informe o Nome")]
     [StringLength(30, ErrorMessage = "O Nome deve possuir no máximo 30 caracteres")]
     public string Name { get; set; }
