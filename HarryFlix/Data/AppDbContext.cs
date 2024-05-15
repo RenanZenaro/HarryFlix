d88821376a39198ba2086fa81fd4cd0bc8c82bf4
RenanZenaro <renanzenaro@hotmail.com>
2024-05-14 22:25:37 -0300

    Criando classe de usuário e código para popular contas.

--- HarryFlix/Data/AppDbContext.cs
+++ HarryFlix/Data/AppDbContext.cs
@@ -1,15 +1,18 @@
 using HarryFlix.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.VisualBasic;
 
 namespace HarryFlix.Data;
 
-public class AppDbContext : DbContext
+public class AppDbContext : IdentityDbContext
 {
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
     {
     }
 
+    public DbSet<AppUser> AppUsers { get; set; }
     public DbSet<Genre> Genres { get; set; }
     public DbSet<Movie> Movies { get; set; }
     public DbSet<MovieGenre> MovieGenres { get; set; }
@@ -34,6 +37,94 @@ public class AppDbContext : DbContext
             .HasOne(mg => mg.Genre)
             .WithMany(m => m.Movies)
             .HasForeignKey(mg => mg.GenreId);
+        #endregion
+
+        #region Popular Usuários
+        // Perfil
+        List<IdentityRole> roles = new()
+        {
+            new IdentityRole()
+            {
+                Id = Guid.NewGuid().ToString(),
+                Name = "Administrador",
+                NormalizedName = "ADMINISTRADOR"
+            },
+            new IdentityRole()
+            {
+                Id = Guid.NewGuid().ToString(),
+                Name = "Usuário",
+                NormalizedName = "USUÁRIO"
+            }
+        };
+        builder.Entity<IdentityRole>().HasData(roles);
+        // User
+        List<IdentityUser> users = new()
+        {
+            new IdentityUser() 
+            {
+                Id = Guid.NewGuid().ToString(),
+                Email = "admin@harryflix.com",
+                NormalizedEmail = "ADMIN@HARRYFLIX.COM",
+                UserName = "Admin",
+                NormalizedUserName = "ADMIN",
+                LockoutEnabled = false,
+                EmailConfirmed = true
+            },
+            new IdentityUser() 
+            {
+                Id = Guid.NewGuid().ToString(),
+                Email = "user@gmail.com",
+                NormalizedEmail = "USER@GMAIL.COM",
+                UserName = "User",
+                NormalizedUserName = "USER",
+                LockoutEnabled = true,
+                EmailConfirmed = true
+            }
+        };
+        foreach (var user in users)
+        {
+            PasswordHasher<IdentityUser> pass = new();
+            user.PasswordHash = pass.HashPassword(user, "@Etec123");
+        }
+        builder.Entity<IdentityUser>().HasData(users);
+        // AppUser
+        List<AppUser> appUsers = new()
+        {
+            new AppUser()
+            {
+                AppUserId = users[0].Id,
+                Name = "Renan Augusto Zenaro do Carmo",
+                Birthday = DateTime.Parse("18/12/2001"),
+                Photo = "/img/usuarios/avatar.png"
+            },
+            new AppUser()
+            {
+                AppUserId = users[1].Id,
+                Name = "Fulaninho",
+                Birthday = DateTime.Parse("01/04/2002"),
+            },
+        };
+        builder.Entity<AppUser>().HasData(appUsers);
+        // Perfil Usuário
+        List<IdentityUserRole<string>> userRoles = new()
+        {
+            new IdentityUserRole<string>()
+            {
+                UserId = users[0].Id,
+                RoleId = roles[0].Id
+            },
+            new IdentityUserRole<string>()
+            {
+                UserId = users[0].Id,
+                RoleId = roles[1].Id
+            },
+            new IdentityUserRole<string>()
+            {
+                UserId = users[1].Id,
+                RoleId = roles[1].Id
+            }
+        };
+        builder.Entity<IdentityUserRole<string>>().HasData(userRoles);
         #endregion
     }
 }
