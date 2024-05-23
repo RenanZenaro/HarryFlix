0d9fd71fe66c319519d8248fa638cea6a7ecfd38
RenanZenaro <renanzenaro@hotmail.com>
2024-05-22 22:09:44 -0300

    Finalizado AccountController e criado Login.

--- HarryFlix/Controllers/AccountController.cs
+++ HarryFlix/Controllers/AccountController.cs
@@ -1,4 +1,5 @@
 using System.Net.Mail;
+using System.Security.Claims;
 using HarryFlix.ViewModels;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -38,12 +39,44 @@ public class AccountController : Controller
     {
         if (ModelState.IsValid)
         {
-            
+            string userName = login.Email;
+            if (IsValidEmail(userName))
+            {
+                var user = await _userManager.FindByEmailAsync(userName);
+                if (user != null)
+                    userName = user.UserName;
+            }
+
+            var result = await _signInManager.PasswordSignInAsync(
+                userName, login.Password, login.RememberMe, lockoutOnFailure: true
+            );
+
+            if (result.Succeeded)
+            {
+                _logger.LogInformation($"Usuário {userName} fez login");
+                return LocalRedirect(login.ReturnUrl);
+            }
+
+            if (result.IsLockedOut)
+            {
+                _logger.LogWarning($"Usuário {userName} foi bloqueado");
+                ModelState.AddModelError(string.Empty, "Conta Bloqueada! Aguarde alguns minutos para continuar");
+            }
+            else
+                ModelState.AddModelError(string.Empty, "Usuário e/ou Senha Inválidos");
         }
         return View(login);
     }
 
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Logout()
+    {
+        _logger.LogInformation($"Usuário {ClaimTypes.Email} fez logoff");
+        await _signInManager.SignOutAsync();
+        return RedirectToAction("Index", "Home");
+    }
+    
     public IActionResult Error()
     {
         return View("Error!");
